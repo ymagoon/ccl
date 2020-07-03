562e5ffb260620f30444a49a80ed8b31e48823bf
Yitzhak Magoon <ymagoon@gmail.com>
2020-07-03 09:45:58 -0400

    Initial Release

---
+++ Rules/Physician Notification/data.js
@@ -0,0 +1,176 @@
+const list = {
+    "me": 13964820,
+    "relationship": "Troubleshooting",
+    "forceInd": 0,
+    "indications": [
+      {
+        "display": "Accurate output in critically ill patien",
+        "description": "Accurate output in critically ill patient",
+        "codeValue": 2559271907
+      },
+      {
+        "display": "Chemotherapy",
+        "description": "Chemotherapy",
+        "codeValue": 2559271825
+      },
+      {
+        "display": "Hemodialysis or Plasmapheresis",
+        "description": "Hemodialysis orPlasmapheresis",
+        "codeValue": 2559271835
+      },
+      {
+        "display": "Hemodynamic Monitoring",
+        "description": "Hemodynamic Monitoring",
+        "codeValue": 2559271845
+      },
+      {
+        "display": "IV Infusions requiring line",
+        "description": "IV Infusions requiring line",
+        "codeValue": 2559271855
+      },
+      {
+        "display": "Neurogenic bladder or retention",
+        "description": "Neurogenic bladder or retention",
+        "codeValue": 2559271927
+      },
+      {
+        "display": "Palliative Care",
+        "description": "As comfort measure for palliative care or hospice patients",
+        "codeValue": 2559271917
+      },
+      {
+        "display": "Post-op Day 1 or Post-op Day 2",
+        "description": "Post-op Day 1 or Post-op Day 2",
+        "codeValue": 2559271937
+      },
+      {
+        "display": "Promote healing pressure ulcer",
+        "description": "Promote healing Stage III or IV pressure ulcer",
+        "codeValue": 2559271891
+      },
+      {
+        "display": "Relieve Obstruction",
+        "description": "Relieve Obstruction",
+        "codeValue": 2559271865
+      },
+      {
+        "display": "Urologic or other surgery",
+        "description": "Urologic or other surgery on contiguous organs",
+        "codeValue": 2559271875
+      },
+      {
+        "display": "Venous Access",
+        "description": "Venous Access",
+        "codeValue": 2559271947
+      }
+    ],
+    "dynamicLabel": [
+      {
+        "existInd": 0,
+        "createDtTm": 0,
+        "display": ""
+      },
+      {
+        "existInd": 1,
+        "createDtTm": 69487062610000000,
+        "display": "Central Venous"
+      },
+      {
+        "existInd": 1,
+        "createDtTm": 69502435080000000,
+        "display": "Urinary Catheter"
+      }
+    ],
+    "map": [
+      {
+        "insertOrdCd": 4149435,
+        "careOrdCd": 4149429,
+        "dcOrdCd": 4149432,
+        "display": "Arterial"
+      },
+      {
+        "insertOrdCd": 33708007,
+        "careOrdCd": 2780643,
+        "dcOrdCd": 4149440,
+        "display": "Central Venous"
+      },
+      {
+        "insertOrdCd": 2958525,
+        "careOrdCd": 22937729,
+        "dcOrdCd": 20230936,
+        "display": "Urinary Catheter"
+      }
+    ],
+    "orders": [
+      {
+        "orderId": 534905443,
+        "orderMnemonic": "Arterial Line Insertion",
+        "catalogCd": 4149435,
+        "clinDisplay": "07/01/20 7:06:00 PDT, Reason: Hemodynamic Monitoring, Continuous",
+        "orderPhys": "Cerner Test, Nurse Practitioner Cerner",
+        "origOrderDtTm": "2020-07-01T14:06:12.000+00:00",
+        "orderDtTm": "",
+        "startDtTm": "",
+        "stopDtTm": "",
+        "expiresIn": 0,
+        "indicationInd": 0,
+        "type": "",
+        "curTime": "2020-07-02T13:49:11.000+00:00",
+        "minDiff": 0,
+        "hrDiff": 0
+      },
+      {
+        "orderId": 534905445,
+        "orderMnemonic": "Central Line Insertion",
+        "catalogCd": 33708007,
+        "clinDisplay": "Routine, Hemodynamically Unstable",
+        "orderPhys": "Cerner Test, Nurse Practitioner Cerner",
+        "origOrderDtTm": "2020-07-01T14:06:12.000+00:00",
+        "orderDtTm": "",
+        "startDtTm": "",
+        "stopDtTm": "",
+        "expiresIn": 0,
+        "indicationInd": 0,
+        "type": "",
+        "curTime": "2020-07-02T13:49:11.000+00:00",
+        "minDiff": 0,
+        "hrDiff": 0
+      },
+      {
+        "orderId": 534905471,
+        "orderMnemonic": "Arterial Line Care",
+        "catalogCd": 4149429,
+        "clinDisplay": "07/01/20 7:06:49 PDT, Daily, 1, days, Stop date 07/02/20 7:06:49 PDT",
+        "orderPhys": "Cerner Test, Nurse Practitioner Cerner",
+        "origOrderDtTm": "2020-07-01T14:06:49.000+00:00",
+        "orderDtTm": "07/01/20 07:06",
+        "startDtTm": "07/01/20 07:06",
+        "stopDtTm": "07/02/20 07:06",
+        "expiresIn": 17,
+        "indicationInd": 0,
+        "type": "",
+        "curTime": "2020-07-02T13:49:11.000+00:00",
+        "minDiff": 17,
+        "hrDiff": 0
+      },
+      {
+        "orderId": 53490585,
+        "orderMnemonic": "Central Venous Catheter Care",
+        "catalogCd": 2780643,
+        "clinDisplay": "07/01/20 7:06:50 PDT Hemodynamically Unstable, 24, hr, Stop date 07/02/20 7:06:50 PDT, Constant order",
+        "orderPhys": "Cerner Test, Nurse Practitioner Cerner",
+        "origOrderDtTm": "2020-07-01T14:06:50.000+00:00",
+        "orderDtTm": "07/01/20 07:06",
+        "startDtTm": "07/01/20 07:06",
+        "stopDtTm": "07/02/2007:06",
+        "expiresIn": -1422,
+        "indicationInd": 0,
+        "type": "",
+        "curTime": "2020-07-02T13:49:11.000+00:00",
+        "minDiff": -1422,
+        "hrDiff": -23
+      }
+    ]
+  }
+
+//export default list;
---
+++ Rules/Physician Notification/demographics.js
@@ -0,0 +1,14 @@
+const pt = {
+    "name": "AVHTEST, NURSEONE",
+    "age": "75 Years",
+    "sex": "Male",
+    "location": "3N/380/01",
+    "allergies": "Animal Dander,Apple Juice,Ragweed,Shrimp",
+    "dob": "02/01/45",
+    "fin": "20139",
+    "mrn": "645020033",
+    "height": "180 cm (02/18/20)",
+    "weight": "78 kg (02/18/20)"
+  }
+
+//export default pt;
