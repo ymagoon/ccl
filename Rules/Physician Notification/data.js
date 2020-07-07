6aaee351aa221dc5d31890636d5f2a8e2cfd75cb
Yitzhak Magoon <ymagoon@gmail.com>
2020-07-07 13:30:05 -0400

    big update

--- Rules/Physician Notification/data.js
+++ Rules/Physician Notification/data.js
@@ -1,176 +1,238 @@
 const list = {
-    "me": 13964820,
-    "relationship": "Troubleshooting",
-    "forceInd": 0,
-    "indications": [
-      {
-        "display": "Accurate output in critically ill patien",
-        "description": "Accurate output in critically ill patient",
-        "codeValue": 2559271907
-      },
-      {
-        "display": "Chemotherapy",
-        "description": "Chemotherapy",
-        "codeValue": 2559271825
-      },
-      {
-        "display": "Hemodialysis or Plasmapheresis",
-        "description": "Hemodialysis orPlasmapheresis",
-        "codeValue": 2559271835
-      },
-      {
-        "display": "Hemodynamic Monitoring",
-        "description": "Hemodynamic Monitoring",
-        "codeValue": 2559271845
-      },
-      {
-        "display": "IV Infusions requiring line",
-        "description": "IV Infusions requiring line",
-        "codeValue": 2559271855
-      },
-      {
-        "display": "Neurogenic bladder or retention",
-        "description": "Neurogenic bladder or retention",
-        "codeValue": 2559271927
-      },
-      {
-        "display": "Palliative Care",
-        "description": "As comfort measure for palliative care or hospice patients",
-        "codeValue": 2559271917
-      },
-      {
-        "display": "Post-op Day 1 or Post-op Day 2",
-        "description": "Post-op Day 1 or Post-op Day 2",
-        "codeValue": 2559271937
-      },
-      {
-        "display": "Promote healing pressure ulcer",
-        "description": "Promote healing Stage III or IV pressure ulcer",
-        "codeValue": 2559271891
-      },
-      {
-        "display": "Relieve Obstruction",
-        "description": "Relieve Obstruction",
-        "codeValue": 2559271865
-      },
-      {
-        "display": "Urologic or other surgery",
-        "description": "Urologic or other surgery on contiguous organs",
-        "codeValue": 2559271875
-      },
-      {
-        "display": "Venous Access",
-        "description": "Venous Access",
-        "codeValue": 2559271947
-      }
-    ],
-    "dynamicLabel": [
-      {
-        "existInd": 0,
-        "createDtTm": 0,
-        "display": ""
-      },
-      {
-        "existInd": 1,
-        "createDtTm": 69487062610000000,
-        "display": "Central Venous"
-      },
-      {
-        "existInd": 1,
-        "createDtTm": 69502435080000000,
-        "display": "Urinary Catheter"
-      }
-    ],
-    "map": [
-      {
-        "insertOrdCd": 4149435,
-        "careOrdCd": 4149429,
-        "dcOrdCd": 4149432,
-        "display": "Arterial"
-      },
-      {
-        "insertOrdCd": 33708007,
-        "careOrdCd": 2780643,
-        "dcOrdCd": 4149440,
-        "display": "Central Venous"
-      },
-      {
-        "insertOrdCd": 2958525,
-        "careOrdCd": 22937729,
-        "dcOrdCd": 20230936,
-        "display": "Urinary Catheter"
-      }
-    ],
-    "orders": [
-      {
-        "orderId": 534905443,
-        "orderMnemonic": "Arterial Line Insertion",
-        "catalogCd": 4149435,
-        "clinDisplay": "07/01/20 7:06:00 PDT, Reason: Hemodynamic Monitoring, Continuous",
-        "orderPhys": "Cerner Test, Nurse Practitioner Cerner",
-        "origOrderDtTm": "2020-07-01T14:06:12.000+00:00",
-        "orderDtTm": "",
-        "startDtTm": "",
-        "stopDtTm": "",
-        "expiresIn": 0,
-        "indicationInd": 0,
-        "type": "",
-        "curTime": "2020-07-02T13:49:11.000+00:00",
-        "minDiff": 0,
-        "hrDiff": 0
-      },
-      {
-        "orderId": 534905445,
-        "orderMnemonic": "Central Line Insertion",
-        "catalogCd": 33708007,
-        "clinDisplay": "Routine, Hemodynamically Unstable",
-        "orderPhys": "Cerner Test, Nurse Practitioner Cerner",
-        "origOrderDtTm": "2020-07-01T14:06:12.000+00:00",
-        "orderDtTm": "",
-        "startDtTm": "",
-        "stopDtTm": "",
-        "expiresIn": 0,
-        "indicationInd": 0,
-        "type": "",
-        "curTime": "2020-07-02T13:49:11.000+00:00",
-        "minDiff": 0,
-        "hrDiff": 0
-      },
-      {
-        "orderId": 534905471,
-        "orderMnemonic": "Arterial Line Care",
-        "catalogCd": 4149429,
-        "clinDisplay": "07/01/20 7:06:49 PDT, Daily, 1, days, Stop date 07/02/20 7:06:49 PDT",
-        "orderPhys": "Cerner Test, Nurse Practitioner Cerner",
-        "origOrderDtTm": "2020-07-01T14:06:49.000+00:00",
-        "orderDtTm": "07/01/20 07:06",
-        "startDtTm": "07/01/20 07:06",
-        "stopDtTm": "07/02/20 07:06",
-        "expiresIn": 17,
-        "indicationInd": 0,
-        "type": "",
-        "curTime": "2020-07-02T13:49:11.000+00:00",
-        "minDiff": 17,
-        "hrDiff": 0
-      },
-      {
-        "orderId": 53490585,
-        "orderMnemonic": "Central Venous Catheter Care",
-        "catalogCd": 2780643,
-        "clinDisplay": "07/01/20 7:06:50 PDT Hemodynamically Unstable, 24, hr, Stop date 07/02/20 7:06:50 PDT, Constant order",
-        "orderPhys": "Cerner Test, Nurse Practitioner Cerner",
-        "origOrderDtTm": "2020-07-01T14:06:50.000+00:00",
-        "orderDtTm": "07/01/20 07:06",
-        "startDtTm": "07/01/20 07:06",
-        "stopDtTm": "07/02/2007:06",
-        "expiresIn": -1422,
-        "indicationInd": 0,
-        "type": "",
-        "curTime": "2020-07-02T13:49:11.000+00:00",
-        "minDiff": -1422,
-        "hrDiff": -23
-      }
-    ]
-  }
+  "me": 13964820,
+  "relationship": "Troubleshooting",
+  "forceInd": 0,
+  "indications": [
+    {
+      "display": "Acute Urinary Retention",
+      "description": "Acute Urinary Retention",
+      "type": "Urinary Catheter"
+    },
+    {
+      "display": "Critically Ill or Unstable",
+      "description": "Critically Ill or Unstable",
+      "type": "Urinary Catheter"
+    },
+    {
+      "display": "End of Life Care",
+      "description": "End of Life Care",
+      "type": "Urinary Catheter"
+    },
+    {
+      "display": "Epidural/Pain Management Care",
+      "description": "Epidural/Pain Management Care",
+      "type": "Urinary Catheter"
+    },
+    {
+      "display": "Excessive Urine Volume",
+      "description": "Excessive Urine Volume",
+      "type": "Urinary Catheter"
+    },
+    {
+      "display": "Hospice/Palliative Care Measures",
+      "description": "Hospice/Palliative Care Measures",
+      "type": "Urinary Catheter"
+    },
+    {
+      "display": "Immobilization due to Unstable Fracture",
+      "description": "Immobilization due to Unstable Fracture",
+      "type": "Urinary Catheter"
+    },
+    {
+      "display": "Incontinence w/ Sacral or Perineal Wound",
+      "description": "Incontinence w/ Sacral or Perineal Wound",
+      "type": "Urinary Catheter"
+    },
+    {
+      "display": "Intake and Output",
+      "description": "Intake and Output",
+      "type": "Urinary Catheter"
+    },
+    {
+      "display": "Neurogenic Bladder Dysfunction",
+      "description": "Neurogenic Bladder Dysfunction",
+      "type": "Urinary Catheter"
+    },
+    {
+      "display": "Other",
+      "description": "Other",
+      "type": "Urinary Catheter"
+    },
+    {
+      "display": "Surgical Procedure",
+      "description": "Surgical Procedure",
+      "type": "Urinary Catheter"
+    },
+    {
+      "display": "Urinary Obstruction",
+      "description": "Urinary Obstruction",
+      "type": "Urinary Catheter"
+    },
+    {
+      "display": "Hemodynamically Unstable",
+      "description": "Hemodynamically Unstable",
+      "type": "Central Venous"
+    },
+    {
+      "display": "IV Fluids Not for Peripheral Line",
+      "description": "IV Fluids Not for Peripheral Line",
+      "type": "Central Venous"
+    },
+    {
+      "display": "Other (please specify)",
+      "description": "Other (please specify)",
+      "type": "Central Venous"
+    },
+    {
+      "display": "Requires Large Volume IV Infusion",
+      "description": "Requires Large Volume IV Infusion",
+      "type": "Central Venous"
+    },
+    {
+      "display": "Requires Total Parenteral Nutrition",
+      "description": "Requires Total Parenteral Nutrition",
+      "type": "Central Venous"
+    },
+    {
+      "display": "Requires Vaso Active Medication",
+      "description": "Requires Vaso Active Medication",
+      "type": "Central Venous"
+    }
+  ],
+  "dynamicLabel": [
+    {
+      "existInd": 0,
+      "createDtTm": 0,
+      "display": ""
+    },
+    {
+      "existInd": 1,
+      "createDtTm": 69487062610000000,
+      "display": "Central Venous"
+    },
+    {
+      "existInd": 1,
+      "createDtTm": 69502435080000000,
+      "display": "Urinary Catheter"
+    }
+  ],
+  "map": [
+    {
+      "insertOrdCd": 4149435,
+      "insertSynId": 4149436,
+      "careOrdCd": 4149429,
+      "careSynId": 4149430,
+      "dcOrdCd": 4149432,
+      "dcSynId": 4149433,
+      "display": "Arterial"
+    },
+    {
+      "insertOrdCd": 33708007,
+      "insertSynId": 33708013,
+      "careOrdCd": 2780643,
+      "careSynId": 2780644,
+      "dcOrdCd": 4149440,
+      "dcSynId": 4149441,
+      "display": "Central Venous"
+    },
+    {
+      "insertOrdCd": 2958525,
+      "insertSynId": 2958526,
+      "careOrdCd": 22937729,
+      "careSynId": 22937731,
+      "dcOrdCd": 20230936,
+      "dcSynId": 20230938,
+      "display": "Urinary Catheter"
+    }
+  ],
+  "orders": [
+    {
+      "orderId": 534905443,
+      "orderMnemonic": "Arterial Line Insertion",
+      "catalogCd": 4149435,
+      "clinDisplay": "07/01/20 7:06:00 PDT, Reason: Hemodynamic Monitoring, Continuous",
+      "orderPhys": "Cerner Test, Nurse Practitioner Cerner",
+      "origOrderDtTm": "2020-07-01T14:06:12.000+00:00",
+      "orderDtTm": "",
+      "startDtTm": "",
+      "stopDtTm": "",
+      "expiresIn": 0,
+      "indicationInd": 0,
+      "type": "insert",
+      "curTime": "2020-07-06T20:38:40.000+00:00",
+      "minDiff": 0,
+      "hrDiff": 0
+    },
+    {
+      "orderId": 534905445,
+      "orderMnemonic": "Central Line Insertion",
+      "catalogCd": 33708007,
+      "clinDisplay": "Routine, Hemodynamically Unstable",
+      "orderPhys": "Cerner Test, Nurse Practitioner Cerner",
+      "origOrderDtTm": "2020-07-01T14:06:12.000+00:00",
+      "orderDtTm": "",
+      "startDtTm": "",
+      "stopDtTm": "",
+      "expiresIn": 0,
+      "indicationInd": 0,
+      "type": "insert",
+      "curTime": "2020-07-06T20:38:40.000+00:00",
+      "minDiff": 0,
+      "hrDiff": 0
+    },
+    {
+      "orderId": 534905471,
+      "orderMnemonic": "Arterial Line Care",
+      "catalogCd": 4149429,
+      "clinDisplay": "07/01/20 7:06:49 PDT, Daily, 1, days, Stop date 07/02/20 7:06:49 PDT",
+      "orderPhys": "Cerner Test, Nurse PractitionerCerner",
+      "origOrderDtTm": "2020-07-01T14:06:49.000+00:00",
+      "orderDtTm": "07/01/20 07:06",
+      "startDtTm": "07/01/20 07:06",
+      "stopDtTm": "07/02/20 07:06",
+      "expiresIn": -7591,
+      "indicationInd": 0,
+      "type": "care",
+      "curTime": "2020-07-06T20:38:40.000+00:00",
+      "minDiff": -7591,
+      "hrDiff": -126
+    },
+    {
+      "orderId": 534905485,
+      "orderMnemonic": "Central Venous Catheter Care",
+      "catalogCd": 2780643,
+      "clinDisplay": "07/01/20 7:06:50 PDT Hemodynamically Unstable, 24, hr, Stop date 07/02/20 7:06:50 PDT, Constant order",
+      "orderPhys": "Cerner Test, Nurse Practitioner Cerner",
+      "origOrderDtTm": "2020-07-01T14:06:50.000+00:00",
+      "orderDtTm": "07/01/20 07:06",
+      "startDtTm": "07/01/20 07:6",
+      "stopDtTm": "07/02/20 07:06",
+      "expiresIn": -6151,
+      "indicationInd": 0,
+      "type": "care",
+      "curTime": "2020-07-06T20:38:40.000+00:00",
+      "minDiff": -6151,
+      "hrDiff": -102
+    },
+    {
+      "orderId": 534911619,
+      "orderMnemonic": "Urinary Catheter Care",
+      "catalogCd": 22937729,
+      "clinDisplay": "07/06/20 12:17:00 PDT, Indwelling, Neurogenic Bladder Dysfunction, 24, hr, Stop date 07/07/20 12:16:00 PDT, Constant order, 07/06/20 12:17:00 PDT",
+      "orderPhys": "AVHTest, Physician",
+      "origOrderDtTm": "2020-07-06T19:24:13.000+00:00",
+      "orderDtTm": "07/06/20 12:24",
+      "startDtTm": "07/06/20 12:17",
+      "stopDtTm": "07/07/20 12:16",
+      "expiresIn": 0,
+      "indicationInd": 0,
+      "type": "care",
+      "curTime": "2020-07-06T20:38:40.000+00:00",
+      "minDiff": 0,
+      "hrDiff": 0
+    }
+  ]
+}
 
 //export default list;
+
---
+++ Rules/Physician Notification/data2.js
@@ -0,0 +1,286 @@
+const list = {
+  "prsnlId": 13964820,
+  "relationship": "",
+  "map": [
+    {
+      "orders": [
+        {
+          "display": "Arterial Line Insertion",
+          "catalogCd": 4149435,
+          "synonimId": 4149436,
+          "orderSentences": []
+        },
+        {
+          "display": "Arterial Line Care",
+          "catalogCd": 4149429,
+          "synonimId": 4149430,
+          "orderSentences": []
+        },
+        {
+          "display": "Arterial Line Discontinue",
+          "catalogCd": 4149432,
+          "synonimId": 4149433,
+          "orderSentences": []
+        }
+      ],
+      "dynamicLabel": [
+        {
+          "existInd": 0,
+          "createDtTm": "0000-00-00T00:00:00.000+00:00",
+          "display": ""
+        }
+      ]
+    },
+    {
+      "orders": [
+        {
+          "display": "Central LineInsertion",
+          "catalogCd": 33708007,
+          "synonimId": 33708013,
+          "orderSentences": []
+        },
+        {
+          "display": "Central Venous Catheter Care",
+          "catalogCd": 2780643,
+          "synonimId": 2780644,
+          "orderSentences": [
+            {
+              "indication": "Hemodynamically Unstable",
+              "orderSentenceId": 2651778705
+            },
+            {
+              "indication": "IV Fluids Not for Peripheral Line",
+              "orderSentenceId": 2651778711
+            },
+            {
+              "indication": "Requires Large Volume IV Infusion",
+              "orderSentenceId": 2651778723
+            },
+            {
+              "indication": "Requires Total Parenteral Nutrition",
+              "orderSentenceId": 2651778729
+            },
+            {
+              "indication": "Requires Vaso Active Medication",
+              "orderSentenceId": 2651778717
+            }
+          ]
+        },
+        {
+          "display": "Central Venous Catheter Discontinue",
+          "catalogCd": 4149440,
+          "synonimId": 4149441,
+          "orderSentences": []
+        }
+      ],
+      "dynamicLabel": [
+        {
+          "existInd": 1,
+          "createDtTm": "2020-03-09T20:11:01.000+00:00",
+          "display": "Central Venous"
+        }
+      ]
+    },
+    {
+      "orders": [
+        {
+          "display": "Urinary Catheter Insertion",
+          "catalogCd": 2958525,
+          "synonimId": 2958526,
+          "orderSentences": [
+            {
+              "indication": "Acute Urinary Retention",
+              "orderSentenceId": 59888669
+            },
+            {
+              "indication": "Critically Ill or Unstable",
+              "orderSentenceId": 59888675
+            },
+            {
+              "indication": "End of Life Care",
+              "orderSentenceId": 59888681
+            },
+            {
+              "indication": "Epidural/Pain Management Care",
+              "orderSentenceId": 59888693
+            },
+            {
+              "indication": "Excessive Urine Volume",
+              "orderSentenceId": 59888699
+            },
+            {
+              "indication": "Hospice/Palliative Care Measures",
+              "orderSentenceId": 2651778981
+            },
+            {
+              "indication": "Immobilization due to Unstable Fracture",
+              "orderSentenceId": 59888705
+            },
+            {
+              "indication": "Incontinence w/ Sacral or Perineal Wound",
+              "orderSentenceId": 59888711
+            },
+            {
+              "indication": "Intake and Output",
+              "orderSentenceId": 59888717
+            },
+            {
+              "indication": "Neurogenic Bladder Dysfunction",
+              "orderSentenceId": 59888723
+            }
+          ]
+        },
+        {
+          "display": "Urinary Catheter Care",
+          "catalogCd": 22937729,
+          "synonimId": 22937731,
+          "orderSentences": [
+            {
+              "indication": "Acute Urinary Retention",
+              "orderSentenceId": 2651778969
+            },
+            {
+              "indication": "Critically Ill or Unstable",
+              "orderSentenceId": 2651778975
+            },
+            {
+              "indication": "End of Life Care",
+              "orderSentenceId": 2651765723
+            },
+            {
+              "indication": "Epidural/Pain Management Care",
+              "orderSentenceId": 2651765729
+            },
+            {
+              "indication": "Excessive Urine Volume",
+              "orderSentenceId": 2651765735
+            },
+            {
+              "indication": "Hospice/Palliative Care Measures",
+              "orderSentenceId": 2651765741
+            },
+            {
+              "indication": "Immobilization due to Unstable Fracture",
+              "orderSentenceId": 2651765747
+            },
+            {
+              "indication": "Incontinence w/ Sacral or Perineal Wound",
+              "orderSentenceId": 2651765753
+            },
+            {
+              "indication": "Intake and Output",
+              "orderSentenceId": 2651765759
+            },
+            {
+              "indication": "Neurogenic Bladder Dysfunction",
+              "orderSentenceId": 2651765765
+            }
+          ]
+        },
+        {
+          "display": "Urinary Catheter Discontinue",
+          "catalogCd": 20230936,
+          "synonimId": 20230938,
+          "orderSentences": []
+        }
+      ],
+      "dynamicLabel": [
+        {
+          "existInd": 1,
+          "createDtTm": "2020-03-27T15:11:48.000+00:00",
+          "display": "Urinary Catheter"
+        }
+      ]
+    }
+  ],
+  "orders": [
+    {
+      "orderId": 534905443,
+      "orderMnemonic": "Arterial Line Insertion",
+      "catalogCd": 4149435,
+      "clinDisplay": "07/01/20 7:06:00 PDT, Reason: Hemodynamic Monitoring, Continuous",
+      "orderPhys": "Cerner Test, Nurse Practitioner Cerner",
+      "origOrderDtTm": "2020-07-01T14:06:12.000+00:00",
+      "orderDtTm": "",
+      "startDtTm": "",
+      "stopDtTm": "",
+      "expiresIn": 0,
+      "indicationInd": 0,
+      "type": "insert",
+      "curTime": "2020-07-07T03:56:54.000+00:00",
+      "minDiff": 0,
+      "hrDiff": 0
+    },
+    {
+      "orderId": 534905445,
+      "orderMnemonic": "Central Line Insertion",
+      "catalogCd": 33708007,
+      "clinDisplay": "Routine, Hemodynamically Unstable",
+      "orderPhys": "Cerner Test, Nurse Practitioner Cerner",
+      "origOrderDtTm": "2020-07-01T14:06:12.000+00:00",
+      "orderDtTm": "",
+      "startDtTm": "",
+      "stopDtTm": "",
+      "expiresIn": 0,
+      "indicationInd": 0,
+      "type": "insert",
+      "curTime": "2020-07-07T03:56:54.000+00:00",
+      "minDiff": 0,
+      "hrDiff": 0
+    },
+    {
+      "orderId": 534905471,
+      "orderMnemonic": "Arterial Line Care",
+      "catalogCd": 4149429,
+      "clinDisplay": "07/01/20 7:06:49 PDT, Daily, 1, days, Stop date 07/02/20 7:06:49 PDT",
+      "orderPhys": "Cerner Test, Nurse Practitioner Cerner",
+      "origOrderDtTm": "2020-07-01T14:06:49.000+00:00",
+      "orderDtTm": "07/01/20 07:06",
+      "startDtTm": "07/01/20 07:06",
+      "stopDtTm": "07/02/20 07:06",
+      "expiresIn": -8030,
+      "indicationInd": 0,
+      "type": "care",
+      "curTime": "2020-07-07T03:56:54.000+00:00",
+      "minDiff": -8030,
+      "hrDiff": -133
+    },
+    {
+      "orderId": 534905485,
+      "orderMnemonic": "Central Venous Catheter Care",
+      "catalogCd": 2780643,
+      "clinDisplay": "07/01/20 7:06:50 PDT Hemodynamically Unstable, 24, hr, Stop date 07/02/20 7:06:50 PDT, Constant order",
+      "orderPhys": "Cerner Test, Nurse Practitioner Cerner",
+      "origOrderDtTm": "2020-07-01T14:06:50.000+00:00",
+      "orderDtTm": "07/01/20 07:06",
+      "startDtTm": "07/01/20 07:06",
+      "stopDtTm": "07/02/20 07:06",
+      "expiresIn": -6590,
+      "indicationInd": 0,
+      "type": "care",
+      "curTime": "2020-07-07T03:56:54.000+00:00",
+      "minDiff": -6590,
+      "hrDiff": -109
+    },
+    {
+      "orderId": 534911619,
+      "orderMnemonic": "Urinary Catheter Care",
+      "catalogCd": 22937729,
+      "clinDisplay": "07/06/20 12:17:00 PDT, Indwelling, Neurogenic Bladder Dysfunction, 24, hr, Stop date 07/07/20 12:16:00 PDT, Constant order, 07/06/20 12:17:00 PDT",
+      "orderPhys": "AVHTest, Physician",
+      "origOrderDtTm": "2020-07-06T19:24:13.000+00:00",
+      "orderDtTm": "07/06/20 12:24",
+      "startDtTm": "07/06/20 12:17",
+      "stopDtTm": "07/07/20 12:16",
+      "expiresIn": 0,
+      "indicationInd": 0,
+      "type": "care",
+      "curTime": "2020-07-07T03:56:54.000+00:00",
+      "minDiff": 0,
+      "hrDiff": 0
+    }
+  ]
+}
+
+//export default list;
+
